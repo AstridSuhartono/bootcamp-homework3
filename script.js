9b7c6ab2615de5bca644aa38cf5ed1e3dfd3c1d3
Astrid Suhartono <astrid@yahoo.com>
2020-03-01 22:36:22 +1030

    add codes to set the password length and validate the password length

--- script.js
+++ script.js
@@ -3,17 +3,36 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz",
   uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numbers = "0123456789",
   punctuation = "!@#$%^&*()_+~`|}{[]:;?><,./-=",
-  lowercaseInput = document.getElementById("lowercase"),
+  /* lowercaseInput = document.getElementById("lowercase"),
   uppercaseInput = document.getElementById("uppercase"),
   punctuationInput = document.getElementById("punctuation"),
   numbersInput = document.getElementById("numbers"),
   lengthInput = document.getElementById("length"),
   passwordFeild = document.getElementById("pass-field"),
   generateButton = document.getElementById("generate"),
-  copyButton = document.getElementById("copy"),
-  plength,
+  copyButton = document.getElementById("copy"), */
+  confirmLowercase,
+  confirmUppercase,
+  confirmSpecialChar,
+  confirmNumber,
+  confirmLength,
+  passwordLength,
   userPassword,
   passwordCharSet;
+
+  function generatePassword(){
+    userPassword = "";
+    passwordCharSet ="";
+    //set the length of the password and validate the length input
+    confirmlength = Number(prompt("Please enter you password length between 8 - 128 characters"))
+    while (confirmlength != isNaN || confirmLength < 8 || confirmLength > 128 || confirmLength == null){
+      alert("Not valid, please enter valid number and length")
+      confirmlength = Number(prompt("Please enter you password length between 8 - 128 characters"))
+    }
+    //
+    confirmLowercase = confirm("Include lowercase characters?")
+
+  }
 
   // Assignment Code
 var generateBtn = document.querySelector("#generate");
